108f9850e1d240e860c59ba4143799f735d799f8
Nico <n.rossi.studio@gmail.com>
2026-01-10 17:56:39 +0100

    -Depyl.jsx Populated with project's description, navbar, footer and pictures

--- src/pages/Depyl.jsx
+++ src/pages/Depyl.jsx
@@ -1,10 +1,35 @@
+//Importo i componenti
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+//Importo le immagini
+import depylLogo from '../assets/images/Projects-img/Depyl_Logo_Clear.png';
+import depylPreview from '../assets/images/Projects-img/Depyl-macbook-preview.gif';
+
 function Depyl () {
 
     return(
-
+        <>
+        <Navbar/>
         <div>
-            <h1>Page Depyl</h1>
+            <h1>Depyl</h1>
+            <img className="project-logo" src={depylLogo} alt="Logo Depyl"/>
+            <h3>Water filters</h3>
+          <p>🛠️ Build a Web Page that epresents the Brand.
+          👨🏻‍💻 Craft a professional and engaging web page that showcases the essence of a company. Here are the key elements your page should include to stand out:</p>
+          <ul> 
+            <li>✍️ Company Name: Highlight the brand clearly and prominently.</li>
+            <li>🎨 Company Logo: Create a simple yet eye-catching logo</li>
+            <li>🧑‍🧑‍🧒‍🧒 Social Media Links: Add social icons and link them to general platforms (e.g., facebook.com, instagram.com).</li>
+            <li>🎯 Mission Statement: Write a concise, impactful sentence that captures the company’s mission and vision.</li>
+            <li>🧩 Background Aligned with Brand Values: Choose a background that visually reflects the company's identity and values</li>
+          </ul>
+          <img className="depyl-preview" src={depylPreview} alt="Macbook displaying Depyl landing page" />
         </div>
+
+        <Footer />
+        </>
+        
     );
 }
 
