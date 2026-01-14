c092f0f2eee61057543060c8a1d053a98af63b93
Nico <n.rossi.studio@gmail.com>
2026-01-14 12:52:28 +0100

    ProjectCard.jsx renamed

--- src/components/ProjectCArd.jsx
+++ src/components/ProjectCArd.jsx
@@ -2,25 +2,26 @@
 //Importo le props di React per la navigazione
 import {useNavigate} from 'react-router-dom';
 
-function ProjectCard (project) {
+function ProjectCard ({project}) {
+  
   const navigate =useNavigate();  
   return (
         <div className="project-card">
           <img className="project-card-logo"
           src={project.logo}
-          alt={`${project.title}logo`}
+          alt={`${project.title} logo`}
           />
           <h3 className="project-card-title">
             {project.title}
           </h3>
           <div className="project-card-tags">
             {(project.tags || []).map((tag,index) => (
-              <span key={index} clasName="card-tag">
+              <span key={index} className="card-tag">
                 {tag}
               </span>
             ))}
           </div>
-          <p className="project-card-descrption">{project.description}</p>
+          <p className="project-card-description">{project.description}</p>
           <button className="project-card-button"
             onClick={()=> navigate(`/progetto/${project.id}`)}> 
             Go to project 
